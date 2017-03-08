fecddf057d328c3da0d8cb3c74f53067e6750369
green <vgrinyov@gmail.com>
2017-03-08 21:13:14 +0200

    add model HotelBooking

---
+++ src/main/java/com/grinyov/model/HotelBooking.java
@@ -0,0 +1,40 @@
+package com.grinyov.model;
+
+/**
+ * Created by vgrinyov.
+ */
+public class HotelBooking {
+    private String hotelName;
+    private double pricePerNight;
+    private int numOfNight;
+
+    public HotelBooking(String hotelName, double pricePerNight, int numOfNight) {
+        this.hotelName = hotelName;
+        this.pricePerNight = pricePerNight;
+        this.numOfNight = numOfNight;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public void setPricePerNight(double pricePerNight) {
+        this.pricePerNight = pricePerNight;
+    }
+
+    public int getNumOfNight() {
+        return numOfNight;
+    }
+
+    public void setNumOfNight(int numOfNight) {
+        this.numOfNight = numOfNight;
+    }
+}
