18ef33a373c267e8d9076f52fce06be0a664b00c
green <vgrinyov@gmail.com>
2017-03-11 00:56:49 +0200

    add angular module

--- src/main/resources/static/js/app.module.js
+++ src/main/resources/static/js/app.module.js
@@ -1,3 +1,17 @@
 /**
  * Created by green.
  */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('BookingsController', BookingsController);
+
+    BookingsController.$inject = [];
+
+    function BookingsController() {
+        var vw = this;
+    }
+});
