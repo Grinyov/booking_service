8999e7da35d567b889be93f533af706111d196a9
green <vgrinyov@gmail.com>
2017-03-11 01:19:56 +0200

    mirror db methods to js

--- src/main/resources/static/js/app.module.js
+++ src/main/resources/static/js/app.module.js
@@ -11,7 +11,44 @@
 
     BookingsController.$inject = [];
 
-    function BookingsController() {
+    function BookingsController($http) {
         var vw = this;
+        vm.getAll = getAll;
+        vm.getAffordable = getAffordable;
+        vm.deleteBooking = deleteBooking;
+
+
+        vm.bookings = [];
+
+        init();
+
+        function init() {
+            getAll();
+        }
+
+        function getAll() {
+            var url = "/bookings/all";
+            var bookingsPromise =$htpp.get(url);
+            bookingsPromise.then(function (response) {
+                vm.bookings = response.data;
+            });
+        }
+
+        function getAffordable() {
+            var url = "/bookings/affordable/" + 100;
+            var bookingsPromise =$htpp.get(url);
+            bookingsPromise.then(function (response) {
+                vm.bookings = response.data;
+            });
+        }
+
+        function deleteBooking(id) {
+            var url = "/bookings/delete/" + id;
+            $http.post(url).then(function (response) {
+                vm.bookings = response.data;
+            });
+        }
+
+
     }
 });
